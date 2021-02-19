da309c4c0bddeeba3bbcc0ebebd4f698093aff7d
Rodrigo Labrador <rodrigo.labrador@movetia.com>
2021-02-19 17:17:30 +0100

    Initial Commit

---
+++ IOSKELETON_LOGIN/IOSKELETON_LOGINApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSKELETON_LOGINApp.swift
+//  IOSKELETON_LOGIN
+//
+//  Created by Rodrigo Labrador on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct IOSKELETON_LOGINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
